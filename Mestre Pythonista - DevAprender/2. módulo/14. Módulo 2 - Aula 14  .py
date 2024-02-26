d4bab78dfd43a95cbcd61c19eac69124efef5aad
Arthur Fonseca <144601596+kiraDarthur@users.noreply.github.com>
2024-02-26 12:24:25 +0000

    Delete Mestre Pythonista - DevAprender directory

--- Mestre Pythonista - DevAprender/2. módulo/14. Módulo 2 - Aula 14  .py
+++
@@ -1,8 +0,0 @@
-print('Olá')
-
-def calcular_preco_combo(pizza, refrigerante):
-    total = pizza + refrigerante
-    print(total)
-
-calcular_preco_combo(30, 'vinte reais')
-print('Programa finalizado')
