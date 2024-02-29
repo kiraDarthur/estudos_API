b0bbcba59470dadf558c775ac2e6d84c3b25c0cc
kiraDarthur <arthurandrade8713@gmail.com>
2024-02-29 10:42:19 +0000

    att projeto

--- banco de dados/estrutura_banco_de_dados.py
+++ banco de dados/estrutura_banco_de_dados.py
@@ -1,14 +1,39 @@
 from flask import Flask
-from  flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-#Criar um APi flask
+# Criar um APi Flask
 app = Flask(__name__)
-#criar um isntancia de sqlalchemy
+
+# Configurar SQLAlchemy
 app.config['SECRET_KEY'] = 'FSD2323f#$!SAH'
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
-db:SQLAlchemy
-#Definir a estrtura da tabela Postagem
 
+# Definir a estrutura da tabela Autor
+class Autor(db.Model):
+    __tablename__ = 'autor'
+    id_autor = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    senha = db.Column(db.String(100), nullable=False)
+    admin = db.Column(db.Boolean, default=False)
+    postagens = db.relationship('Postagem', backref='autor', lazy=True)
 
-#Definir a estrutura da tabela Autor
+# Definir a estrutura da tabela Postagem
+class Postagem(db.Model):
+    __tablename__ = 'postagem'
+    id_postagem = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    conteudo = db.Column(db.Text, nullable=False)
+    id_autor = db.Column(db.Integer, db.ForeignKey('autor.id_autor'), nullable=False)
+
+if __name__ == '__main__':
+    # Executar o comando para criar o banco de dados dentro do contexto da aplicação Flask
+    with app.app_context():
+        # Criar o banco de dados
+        db.create_all()
+
+        # Criar usuários administradores
+        autor = Autor(nome='Jhonatan', email='jhonatan@email.com', senha='123456', admin=True)
+        db.session.add(autor)
+        db.session.commit()
